02d8ddd8b867e8ffeb07189f95093b0c687d7099
zjpei <zjpei@microport.com>
2018-04-16 16:44:10 +0800

    Add strcpy.

--- string.c
+++ string.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <stddef.h>
 
+#define MAX_STR_NUM     10
+#define MAX_STR_SIZE    10
+
+char dst[MAX_STR_NUM][MAX_STR_SIZE] = {{'\0',},};
+
 void main ( int argc, char **argv)
 {
     int index = 0;
@@ -11,6 +16,31 @@ void main ( int argc, char **argv)
     for(index = 0; index < argc; index++)
     {
         printf("argv[%d] = %s, Length = %d \n", index+1, *(argv+index), strlen(*(argv+index)));
+    }
+
+    printf("The size of dst is %d, and the size of dst[0] is %d\n", sizeof(dst), sizeof(dst[0]));
+
+    for(index = 0; index < argc; index++)
+    {
+        if(index >= MAX_STR_NUM)
+        {
+            printf("The input strings is more than the strorage num.\n");
+
+            break;
+        }
+
+        if(strlen(*(argv+index)) > MAX_STR_SIZE-1)
+        {
+            printf("dst[%d] the input string is longer than the storage area.\n", index+1);
+        }
+        else
+        {
+            printf("dst[%d] befor strcpy is %s\n", index+1, dst[index]);
+
+            strcpy(dst[index], *(argv+index));
+
+            printf("dst[%d] after strcpy is %s\n", index+1, dst[index]);
+        }
     }
   
     printf("The length of int is %d bytes.\n", sizeof(int));
